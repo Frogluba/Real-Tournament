afc4f96b6eb8950ef0e564482a2ff366dfbfcc30
7700s <146203655+Frogluba@users.noreply.github.com>
2024-02-14 19:18:34 +0200

    Bazooka bby

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public float speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime ;
+
+    }
+
+     void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
