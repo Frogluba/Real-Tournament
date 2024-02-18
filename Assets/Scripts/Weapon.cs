00840faab1152c9939ebd01484cbcf43eb18a487
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-18 12:16:18 +0200

    Weapon

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,22 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    public GameObject bulletPrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GameObject bulletPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Instantiate(bulletPrefab,transform.position, transform.rotation);
-        }
-    }
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Instantiate(bulletPrefab,transform.position,transform.rotation);
+		}
+	}
 }
