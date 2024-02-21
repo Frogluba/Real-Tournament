e10de0c4c89274aab9b0a4d2c15d84359899e819
Frogluba <146203655+Frogluba@users.noreply.github.com>
2024-02-21 18:47:07 +0200

    Wall holes + bounce

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,6 +6,8 @@ public class Rocket : MonoBehaviour
 {
     public float speed = 20;
     public GameObject explosionPrefab;
+    public GameObject hitPrefab;
+    public int bounces;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +23,19 @@ public class Rocket : MonoBehaviour
 
      void OnCollisionEnter(Collision collision)
     {
-        Destroy(gameObject);
+        //Destroy(gameObject);
+        if (bounces == 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.forward = collision.contacts[0].normal;
+        }
+        var obj = Instantiate(hitPrefab, transform.position, transform.rotation);
+        obj.transform.position = collision.contacts[0].point + transform.forward * 0.2f;
+        bounces--;
+        
         var health = collision.gameObject.GetComponent<Healt>();
         if (health != null)
         {
